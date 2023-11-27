552bf88930a76eac2718e66edcb82dd89d7da947
Your Name <you@example.com>
2023-11-28 02:33:02 +0300

    change 6

--- main.js
+++ main.js
@@ -1,106 +1,88 @@
-let menu__link1 = document.getElementById('menu__link')
+"use strict"
+let menu__link = document.getElementById('menu__link')
 let menu = document.getElementById('menu')
 let div1 = document.getElementById('div1')
 let div2 = document.getElementById('div2')
 let div3 = document.getElementById('div3')
-menu.style.display = 'none'
-const Click = () => {
-    if (menu.style.display === 'none') {
-        menu.style.display = 'block'
-        div1.classList.add('div1')
-        div2.classList.add('div2')
-        div3.classList.add('div3')
-        classList.add('animation')
-    } else {
-        menu.style.display = 'none'
-        div1.classList.remove('div1')
-        div2.classList.remove('div2')
-        div3.classList.remove('div3')
-    }
+ 
 
-}
+menu__link.addEventListener("click", () => {
+    menu.classList.toggle('navbar__menu1')
+    div1.classList.toggle('div1')
+    div2.classList.toggle('div2')
+    div3.classList.toggle('div3')
+
+})
 
 
 let lessons = document.getElementById('lessons')
-let video = document.getElementById('video')
-
-function show_video() {
-    if (video.style.display === 'none') {
-        video.style.display = 'block'
-
-    } else {
-        video.style.display = 'none'
-    }
-}
-
-let video2 = document.getElementById('video2')
 let lessons2 = document.getElementById('lessons2')
-function show_video2() {
-    if (video2.style.display === 'none') {
-        video2.style.display = 'block'
-    } else {
-        video2.style.display = 'none'
-    }
-}
-
-let show__list = document.getElementById('show__list')
-lessons.style.display = 'none'
-
-show__list.addEventListener("click", () => {
-    if (lessons.style.display === 'none') {
-        lessons.style.display = 'block'
-    } else {
-        lessons.style.display = 'none'
-
-    } 
-    if (lessons2.style.display === 'none') {
-        lessons2.style.display = 'block'
-    } else {
-        lessons2.style.display = 'none'
-
-    } 
-    
-})
-
-
-let lessons3 = document.getElementById('lessons3')
-let video3 = document.getElementById('video3')
-
-function show_video3() {
-    if (video3.style.display === 'none') {
-        video3.style.display = 'block'
-
-    } else {
-        video3.style.display = 'none'
-    }
-}
-
-let video4 = document.getElementById('video4')
-let lessons4 = document.getElementById('lessons4')
-function show_video4() {
-    if (video4.style.display === 'none') {
-        video4.style.display = 'block'
-    } else {
-        video4.style.display = 'none'
-    }
-}
+ 
 
 let show__list2 = document.getElementById('show__list2')
 
-show__list2.addEventListener("click", () => {
-    if (lessons3.style.display === 'none') {
-        lessons3.style.display = 'block'
-    } else {
-        lessons3.style.display = 'none'
-
-    } 
-    if (lessons4.style.display === 'none') {
-        lessons4.style.display = 'block'
-    } else {
-        lessons4.style.display = 'none'
-
-    } 
-    
+document.getElementById('show__list').addEventListener("click", () => {
+    lessons.classList.toggle('show_lesson')
 })
+show__list2.addEventListener("click", () => {
+    lessons2.classList.toggle('show_lesson')
+})
+document.getElementById("show__list3").addEventListener("click", () => {
+    document.getElementById("lessons3").classList.toggle('show_lesson')
+})
+document.getElementById("show__list5").addEventListener("click", () => {
+    document.getElementById("lessons5").classList.toggle('show_lesson')
+})
+document.getElementById("show__list6").addEventListener("click", () => {
+    document.getElementById("lessons6").classList.toggle('show_lesson')
+})
+document.getElementById("show__list7").addEventListener("click", () => {
+    document.getElementById("lessons7").classList.toggle('show_lesson')
+})
+document.getElementById("show__list8").addEventListener("click", () => {
+    document.getElementById("lessons8").classList.toggle('show_lesson')
+})
+document.getElementById("show__list9").addEventListener("click", () => {
+    document.getElementById("lessons9").classList.toggle('show_lesson')
+})
+document.getElementById("show__list10").addEventListener("click", () => {
+    document.getElementById("lessons10").classList.toggle('show_lesson')
+})
+document.getElementById("show__list11").addEventListener("click", () => {
+    document.getElementById("lessons11").classList.toggle('show_lesson')
+})
+document.getElementById("show__list12").addEventListener("click", () => {
+    document.getElementById("lessons12").classList.toggle('show_lesson')
+})
+document.getElementById("show__list13").addEventListener("click", () => {
+    document.getElementById("lessons13").classList.toggle('show_lesson')
+})
+document.getElementById("show__list14").addEventListener("click", () => {
+    document.getElementById("lessons14").classList.toggle('show_lesson')
+})
+document.getElementById("show__list15").addEventListener("click", () => {
+    document.getElementById("lessons15").classList.toggle('show_lesson')
+})
+document.getElementById("show__list16").addEventListener("click", () => {
+    document.getElementById("lessons16").classList.toggle('show_lesson')
+})
+document.getElementById("show__list17").addEventListener("click", () => {
+    document.getElementById("lessons17").classList.toggle('show_lesson')
+})
+document.getElementById("show__list18").addEventListener("click", () => {
+    document.getElementById("lessons18").classList.toggle('show_lesson')
+})
+document.getElementById("show__list19").addEventListener("click", () => {
+    document.getElementById("lessons19").classList.toggle('show_lesson')
+})
+document.getElementById("show__list20").addEventListener("click", () => {
+    document.getElementById("lessons20").classList.toggle('show_lesson')
+})
+
+
+ 
+
+
+
 
 
