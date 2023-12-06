5f7c5057a172aa82b91e3e60f3bfa519609556fe
Your Name <you@example.com>
2023-12-06 13:55:44 +0300

    change 18

--- settings.js
+++ settings.js
@@ -1,14 +1,12 @@
 let burger = document.getElementById("burger")
 let left__bar = document.getElementById("left__bar")
 let back__left = document.getElementById("back__left")
-burger.addEventListener("click", () =>{
-    left__bar.classList.toggle("right")
+// burger.addEventListener("click", () =>{
+//     left__bar.classList.toggle("right")
 
-})
-.back__left.addEventListener("click", () => {
-})
+// })
+
 function left () {
     left__bar.classList.toggle("right")
-
 }
 
